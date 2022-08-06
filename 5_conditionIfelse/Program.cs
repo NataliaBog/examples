8214ffe05c9add16440a9e4597a4433bc3ce9166
Natalia <natashaaa60802@rambler.ru>
2022-08-06 16:24:22 +0500

    5_conditionIfelse

---
+++ 5_conditionIfelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "natali")
+{
+    Console.WriteLine("Ура это же Natali!");
+}
+else
+{
+    Console.Write("привет, ");
+    Console.Write(username);
+}
